c3b0267ae4958f7138c4ac9b8da8f6b9a7a8a0e9
shinkim <joheuns@hanmail.net>
2025-07-23 15:17:44 +0900

    북마크 삭제 기능 구현

--- components/BookmarksPrvider.js
+++ components/BookmarksPrvider.js
@@ -4,8 +4,29 @@ import { dateToStr } from "../utils/util";
 const BookmarkContext = createContext();
 
 export const BookmarksPrvider = ({ children }) => {
-  const [bookmarks, setBookmarks] = useState([]);
-  const lastBookmarkIdRef = useRef(0);
+  const testData = [
+    {
+      id: 1,
+      content: "테니스 치기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 2,
+      content: "리액트 네이티브 공부하기",
+      regDate: dateToStr(new Date()),
+    },
+    {
+      id: 3,
+      content: "북마크 앱 구현하기",
+      regDate: dateToStr(new Date()),
+    },
+  ];
+
+  // const [bookmarks, setBookmarks] = useState([]);
+  // const lastBookmarkIdRef = useRef(0);
+
+  const [bookmarks, setBookmarks] = useState([...testData]);
+  const lastBookmarkIdRef = useRef(testData.length);
 
   const addBookmark = (newContent) => {
     const id = ++lastBookmarkIdRef.current;
@@ -19,8 +40,15 @@ export const BookmarksPrvider = ({ children }) => {
     setBookmarks(newBookmarks);
   };
 
+  const removeBookmark = (id) => {
+    const newBookmark = bookmarks.filter((bookmark) => bookmark.id != id);
+    setBookmarks(newBookmark);
+  };
+
   return (
-    <BookmarkContext.Provider value={{ bookmarks, addBookmark }}>
+    <BookmarkContext.Provider
+      value={{ bookmarks, addBookmark, removeBookmark }}
+    >
       {children}
     </BookmarkContext.Provider>
   );
--- screens/BookmarkListScreen.js
+++ screens/BookmarkListScreen.js
@@ -1,10 +1,32 @@
-import { StyleSheet, Text, View, Pressable } from "react-native";
+import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
 import React, { useContext } from "react";
 import BookmarkContext from "../components/BookmarksPrvider";
-import { ListItem, Button, Icon } from "@rneui/themed";
+import { ListItem, Icon } from "@rneui/themed";
 
 const BookmarkListScreen = ({ route }) => {
-  const { bookmarks } = useContext(BookmarkContext);
+  const { bookmarks, removeBookmark } = useContext(BookmarkContext);
+
+  const handleRemoveBookmark = (id, reset) => {
+    Alert.alert(
+      "삭제 확인",
+      "정말 삭제하시겠습니까?",
+      [
+        {
+          text: "삭제",
+          onPress: () => {
+            removeBookmark(id);
+            reset();
+          },
+          style: "destructive",
+        },
+        { text: "취소", onPress: () => reset(), style: "cancel" },
+      ],
+      {
+        cancelable: true, // 경고창 상자 밖을 클릭하면 경고창 닫힘
+        onDismiss: () => reset(), // 경고창 상자 밖을 클릭한 경우 콜백 함수 실행
+      }
+    );
+  };
 
   return (
     <View style={styles.listContainer}>
@@ -25,7 +47,7 @@ const BookmarkListScreen = ({ route }) => {
               rightContent={(reset) => (
                 <Pressable
                   style={{ ...styles.pressableBtn, backgroundColor: "red" }}
-                  onPress={() => reset()}
+                  onPress={() => handleRemoveBookmark(bookmark.id, reset)}
                 >
                   <Icon name="delete" color="white" />
                   <Text style={styles.btnText}>삭제</Text>
