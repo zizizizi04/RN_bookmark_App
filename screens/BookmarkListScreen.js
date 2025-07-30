44861d094eed1650d10cae5507665340afaf7547
shinkim <joheuns@hanmail.net>
2025-07-30 13:47:10 +0900

    수정을 위한 모달창구현

--- screens/BookmarkListScreen.js
+++ screens/BookmarkListScreen.js
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View, Pressable, Alert } from "react-native";
-import React, { useContext } from "react";
+import { StyleSheet, Text, View, Pressable, Alert, Modal } from "react-native";
+import React, { useContext, useState } from "react";
 import BookmarkContext from "../components/BookmarksPrvider";
 import { ListItem, Icon } from "@rneui/themed";
 
 const BookmarkListScreen = ({ route }) => {
+  const [modalVisible, setModalVisible] = useState(false);
   const { bookmarks, removeBookmark } = useContext(BookmarkContext);
+
+  const openModifyModal = (reset) => {
+    reset();
+    setModalVisible(true);
+  };
+
+  const closeModifyModal = () => {
+    setModalVisible(false);
+  };
 
   const handleRemoveBookmark = (id, reset) => {
     Alert.alert(
@@ -38,7 +48,7 @@ const BookmarkListScreen = ({ route }) => {
               leftContent={(reset) => (
                 <Pressable
                   style={{ ...styles.pressableBtn, backgroundColor: "blue" }}
-                  onPress={() => reset()}
+                  onPress={() => openModifyModal(reset)}
                 >
                   <Icon name="update" color="white" />
                   <Text style={styles.btnText}>수정</Text>
@@ -69,6 +79,20 @@ const BookmarkListScreen = ({ route }) => {
           작성된 북마크가 없습니다.
         </Text>
       )}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <Pressable onPress={closeModifyModal} style={styles.modalContainer}>
+          <View style={styles.modalBox}>
+            <Text style={{ fontSize: 30 }}>수정창</Text>
+          </View>
+        </Pressable>
+      </Modal>
     </View>
   );
 };
@@ -91,6 +115,19 @@ const styles = StyleSheet.create({
   btnText: {
     color: "#fff",
     fontWeight: "bold",
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalBox: {
+    width: "80%",
+    minHeight: 250,
+    borderWidth: 3,
+    borderRadius: 10,
+    backgroundColor: "#fff",
   },
 });
 
